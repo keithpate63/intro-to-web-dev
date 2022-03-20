352145fbd6270c415f16f678bf2c91045b404432
keithpate63 <kbpate@millersville.edu>
2022-03-20 17:19:25 -0400

    Update script.js

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -1,6 +1,6 @@
 // When this function is called, it takes the user's answer to the first
 // question and uses if statements to check whether it's correct.
-function submitAnswerOne() {
+function submitAnswerOne(answeroneinput) {
   // Get answer one's input element
   const answerOneInput = document.getElementById('answer-one-input');
   // Get the text from answer one's input element
@@ -19,13 +19,13 @@ function submitAnswerOne() {
 
 // When this function is called, it takes the user's answer to the second
 // question and uses if statements to check whether it's correct.
-function submitAnswerTwo() {
+function submitAnswerTwo(answertwoinput) {
   // Get answer two's input element
   const answerTwoInput = document.getElementById('answer-two-input');
   // Get the number from answer two's input element
   const answerTwo = answerTwoInput.value;
 
-  if(answerTwo == 42) {
+  if(answerTwo == 22) {
     alert("That's right!");
     hide('question-two');
     show('done');
@@ -35,6 +35,31 @@ function submitAnswerTwo() {
     alert('The answer is lower.')
   }
 }
+  if(answerThree == 'Millersville') {
+    alert("That's right!");
+    hide('question-three');
+    show('question-four');
+  } else if(answerOne == 'Etters') {
+    alert('I used to live there');
+  } else {
+      alert('Wrong! Hint: It is a college town')
+}
+  if(answerFour == 'rabbit') {
+    alert("That's right!");
+    hide('question-four');
+    show('question-five');
+  } else if(answerOne == 'dog') {
+    alert('I wish');
+  } else {
+    alert('It hops')
+}
+  if(answerFive == '2') {
+    alert("That's right!");
+    hide('question-five');
+}   else if(answerOne == '3') {
+    alert('One too many');
+}   else {
+    alert('Only allowed three total people in Millersville')
 
 // Helper function that takes an ID and shows the element with that ID.
 function show(id) {
